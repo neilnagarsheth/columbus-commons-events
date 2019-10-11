08ad65c55326e3bef0e1f304ce5f971592414c4a
neilnagarsheth <neilnagarsheth@gmail.com>
2019-10-11 10:09:53 -0400

    changing the events so that they can be displayed with the block api

--- slack-service/index.js
+++ slack-service/index.js
@@ -1,33 +1,90 @@
+import stringStripHtml from 'string-strip-html'
 import { readByRequest } from '../events-service';
 
 export async function readByCommandRequest(slackRequest) {
   const { text } = slackRequest;
+  const response_type = 'ephemeral';
   try {
     const events = await readByRequest(text);
-    let attachments = [];
-    let responseText = `No events scheduled for ${text}.`;
-    if(events.length) {
-      responseText = `Here\'s whats happening for ${text}.`
-      attachments = getTextAttachmentsFromEvents(events);
+    if(!events.length) {
+      return {
+        response_type,
+        text: `No events scheduled for ${text}`,
+      };
     }
+    const headerText = `Here\'s whats happening for ${text}.`
     return {
-      response_type: "ephemeral",
-      text: responseText,
-      attachments,
+      response_type,
+      blocks: generateBlocksFromEvents(headerText, events)
     };
   } catch(err) {
     console.log(err);
     throw {
-      response_type: "ephemeral",
+      response_type,
       text: 'There was an error, please try again later',
     }
   }
 }
 
-const getTextAttachmentsFromEvents = (events) => {
-  return events.map(event => {
-    return {
-      text: event.long_description,
-    };
-  });
+const generateBlocksFromEvents = (headerText, events) => {
+  let blocks = events.map(event => generateBlocksFromEventProperties(event));
+  return blocks.reduce((mappedBlocks, eventBlock) => [...mappedBlocks, ...eventBlock], [generateTextSectionWithPlainText(headerText)] )
+};
+
+const generateBlocksFromEventProperties = (event) => {
+  const blocks = [];
+  blocks.push(generateDivider());
+  const boldEventName = `*${event.event_name}*`;
+  blocks.push(generateTextSectionWithMarkDown(boldEventName));
+  blocks.push(generateTextSectionWithPlainText(stringStripHtml(event.long_description)));
+  blocks.push(generateContextWithPlainText(`${event.start_time} - ${event.end_time}`));
+  let eventUrl = event.link;
+  if(eventUrl.startsWith('/')) {
+    eventUrl = `http://columbuscommons.org${eventUrl}`;
+  }
+  if(eventUrl) {
+    blocks.push(generateButton('Details', eventUrl));
+  }
+  return blocks;
+}
+
+const generateTextSectionWithPlainText = (text) => {
+  return generateTextSection(text, 'plain_text');
+};
+
+const generateTextSectionWithMarkDown = (text) => {
+  return generateTextSection(text, 'mrkdwn');
+}
+
+const generateTextSection = (text, type) => {
+  return {
+    type: 'section',
+    text: { type, text}
+  };
+}
+
+const generateDivider = () => ({ type: 'divider' });
+
+const generateContextWithPlainText = (text) => {
+  return {
+    type: 'context',
+    elements: [{
+      type: 'plain_text',
+      text,
+    }]
+  };
+}
+
+const generateButton = (text, url) => {
+  return {
+    type: 'actions',
+    elements: [{
+      type: 'button',
+      text: {
+        type: 'plain_text',
+        text,
+      },
+      url,
+    }],
+  }
 };
